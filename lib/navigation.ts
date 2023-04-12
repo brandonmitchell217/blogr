b735273cec9c549ea0216ba4ae28051961df7c82
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-04-11 22:02:32 -0400

    nav links update

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -8,17 +8,17 @@ export const navLinks: NavLinkProps = [
     text: "Product",
     subLinks: [
       { text: "Overview", href: "/product" },
-      { text: "Pricing", href: "/product/pricing" },
-      { text: "Marketplace", href: "/product/marketplace" },
-      { text: "Features", href: "/product/features" },
-      { text: "Integrations", href: "/product/intergrations" },
+      { text: "Pricing", href: "/pricing" },
+      { text: "Marketplace", href: "/marketplace" },
+      { text: "Features", href: "/features" },
+      { text: "Integrations", href: "/intergrations" },
     ],
   },
   {
     text: "Company",
     subLinks: [
       { text: "About", href: "/about" },
-      { text: "Team", href: "/about/team" },
+      { text: "Team", href: "/team" },
       { text: "Blog", href: "/blog" },
       { text: "Careers", href: "/careers" },
     ],
